d3fcb56496a008745593bc2e68df6f488c1d7c5d
Crispin Immanuel <crispinkingslyi@gmail.com>
2025-12-05 09:46:09 -0500

    Added Wix

--- MyComMSI/Properties/AssemblyInfo.cs
+++ MyComMSI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7a16441f-9a1e-4488-acd8-d5673d1701ac")]
